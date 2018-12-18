26c43ee6c345c8737c1d88feb30a99dcb73d39d8
chewydean <30698951+chewydean@users.noreply.github.com>
2018-12-18 18:05:12 -0500

    Update _.js
    Fixed formatting issues, streamlined functions by removing unnecessary variables. Resolved issue in code where inRange was not returning false correctly in all cases

--- lodash/_.js
+++ lodash/_.js
@@ -1,9 +1,9 @@
 const _ = {
+  
   clamp(number, min, max) {
-    let x = Math.max(number, min);
-    let y = Math.min(x, max);
-    return y;
+    return Math.min(Math.max(number, min), max);
   },
+  
   inRange(number, num1, num2) {
     let start = 0;
     let end = 0;
@@ -14,70 +14,64 @@ const _ = {
       end = num2;
       start = num1;
     }
-    if (num1 > num2)
-      {
+    if (num1 > num2) {
         start = num2;
         end = num1;
-      }
-    if (number < start) {
-      return false;
     }
-    if (number > end) {
+    if (number < start || number >= end) {
       return false;
     }
     return true;
-    },
+ 	},
+  
   words(string) {
-    let arr = string.split(' ');
-    return arr;
+    return string.split(' ');
   },
+  
   pad(string, length) {
     if (string.length >= length) {
       return string;
     }
     let diff = length - string.length;
-    let x = Math.floor(diff / 2);
-    let y = diff - x;
-		let paddedString = ' '.repeat(x) + string + ' '.repeat(y)
-    return paddedString;
+    return ' '.repeat(Math.floor(diff / 2)) + string + ' '.repeat(diff - Math.floor(diff / 2));
   },
+  
   has(obj, key) {
-    if (!obj[key])
-      {
+    if (!obj[key]) {
         return false;
-      }
+    }
     return true;
   },
+  
   invert(obj) {
-    obj2 = {};
+   let obj2 = {};
     for (let key in obj) {
-      let originalValue = obj[key];
-      obj2[originalValue] = key;
+      obj2[obj[key]] = key;
     }
     return obj2;
   },
+  
   findKey(obj,predicate) {
     for (let key in obj) {
-      let value = obj[key];
-      let predicateReturnValue = predicate(value);
-      if (predicateReturnValue) {
+      if (predicate(obj[key])) {
         return key;
       }
     }
     return undefined;
   },
+  
   drop(arr, num) {
     if (!num) {
       num = 1;
     }
-    let arr2 = arr.splice(num);
-    return arr2;
+    return arr.splice(num);
   },
+  
 	dropWhile(arr, predicate) {
     let dropNumber = arr.findIndex((element, index) => {!predicate(element, index, arr)}) - 1;
-    let droppedArray = this.drop(arr, dropNumber);
-    return droppedArray;
+    return this.drop(arr, dropNumber);
   },
+  
   chunk(arr, size) {
     if (!size) {
       size = 1;
@@ -89,7 +83,7 @@ const _ = {
     }
     return result;
   }
-  }
+}
 
 // Do not write or modify code below this line.
 module.exports = _;
